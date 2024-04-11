a38df87f2d2c64f88cdd11abd326a2d9c236dc7d
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-04-11 19:26:34 +0800

    [feat] work out correct calculateCosineSimilarity function in Service

--- demo/src/main/java/com/example/demo/service/ArticleService.java
+++ demo/src/main/java/com/example/demo/service/ArticleService.java
@@ -29,53 +29,68 @@ public class ArticleService {
         return articleRepository.findAll();
     }
 
+public void calculateCosineSimilarity() {
+    List<UserClick> userClicks = userClickRepository.findAll();
 
-    public void calculateCosineSimilarity() {
-        List<UserClick> userClicks = userClickRepository.findAll();
+    // 每個話題的點擊次數
+    double usClickLength = 0.0;
+    double twClickLength = 0.0;
+    double dtClickLength = 0.0;
 
-        // 每個話題的點擊次數
-        double usClickLength = 0.0;
-        double twClickLength = 0.0;
-        double dtClickLength = 0.0;
+    for (UserClick userClick : userClicks) {
+        usClickLength += Math.pow(userClick.getUsClick(), 2);
+        twClickLength += Math.pow(userClick.getTwClick(), 2);
+        dtClickLength += Math.pow(userClick.getDtClick(), 2);
+    }
 
-        for (UserClick userClick : userClicks) {
-            usClickLength += Math.pow(userClick.getUsClick(), 2);
-            twClickLength += Math.pow(userClick.getTwClick(), 2);
-            dtClickLength += Math.pow(userClick.getDtClick(), 2);
+    usClickLength = Math.sqrt(usClickLength);
+    twClickLength = Math.sqrt(twClickLength);
+    dtClickLength = Math.sqrt(dtClickLength);
+
+    // 話題間的內積
+    double usTwDotProduct = 0.0;
+    double usDtDotProduct = 0.0;
+    double twDtDotProduct = 0.0;
+
+    for (UserClick userClick : userClicks) {
+        usTwDotProduct += userClick.getUsClick() * userClick.getTwClick();
+        usDtDotProduct += userClick.getUsClick() * userClick.getDtClick();
+        twDtDotProduct += userClick.getTwClick() * userClick.getDtClick();
+    }
+
+    // 計算cosine similarity
+    double usTwSimilarity = usTwDotProduct / (usClickLength * twClickLength);
+    double usDtSimilarity = usDtDotProduct / (usClickLength * dtClickLength);
+    double twDtSimilarity = twDtDotProduct / (twClickLength * dtClickLength);
+
+    // 更新recommend topics
+    for (UserClick userClick : userClicks) {
+        String favoriteTopic = userClick.getFavoriteTopic();
+        String recommendTopic1;
+        String recommendTopic2;
+
+        // 根據favorite_topic決定要比對哪個餘弦相似度
+        switch (favoriteTopic) {
+            case "us":
+                recommendTopic1 = (usDtSimilarity > usTwSimilarity) ? "dt" : "tw";
+                recommendTopic2 = (usDtSimilarity > usTwSimilarity) ? "tw" : "dt";
+                break;
+            case "tw":
+                recommendTopic1 = (usTwSimilarity > twDtSimilarity) ? "us" : "dt";
+                recommendTopic2 = (usTwSimilarity > twDtSimilarity) ? "dt" : "us";
+                break;
+            case "dt":
+                recommendTopic1 = (usDtSimilarity > twDtSimilarity) ? "us" : "tw";
+                recommendTopic2 = (usDtSimilarity > twDtSimilarity) ? "tw" : "us";
+                break;
+            default:
+                recommendTopic1 = "";
+                recommendTopic2 = "";
         }
 
-        System.out.println("usClickLength(pow): " + usClickLength);
-        System.out.println("twClickLength(pow): " + twClickLength);
-        System.out.println("dtClickLength(pow): " + dtClickLength);
-
-        usClickLength = Math.sqrt(usClickLength);
-        twClickLength = Math.sqrt(twClickLength);
-        dtClickLength = Math.sqrt(dtClickLength);
-
-        System.out.println("usClickLength(sqrt): " + usClickLength);
-        System.out.println("twClickLength(sqrt): " + twClickLength);
-        System.out.println("dtClickLength(sqrt): " + dtClickLength);
-
-
-        // 話題間的內積
-        double usTwDotProduct = 0.0;
-        double usDtDotProduct = 0.0;
-        double twDtDotProduct = 0.0;
-
-        for (UserClick userClick : userClicks) {
-            usTwDotProduct += userClick.getUsClick() * userClick.getTwClick();
-            usDtDotProduct += userClick.getUsClick() * userClick.getDtClick();
-            twDtDotProduct += userClick.getTwClick() * userClick.getDtClick();
+        userClick.setRecommendTopic1(recommendTopic1);
+        userClick.setRecommendTopic2(recommendTopic2);
+        userClickRepository.save(userClick);
         }
-
-        // 計算cosine similarity
-        double usTwSimilarity = usTwDotProduct / (usClickLength * twClickLength);
-        double usDtSimilarity = usDtDotProduct / (usClickLength * dtClickLength);
-        double twDtSimilarity = twDtDotProduct / (twClickLength * dtClickLength);
-
-        // 結果
-        System.out.println("餘弦相似度(US, TW): " + usTwSimilarity);
-        System.out.println("餘弦相似度(US, DT): " + usDtSimilarity);
-        System.out.println("餘弦相似度(TW, DT): " + twDtSimilarity);
     }
 }
