529b924f11c7bae3fc3804c005fad4197f9cab21
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-04-13 13:49:18 +0800

    [feat] add function prototype (6) - @Scheduled simple construction

--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -6,7 +6,10 @@ import com.example.demo.model.user.UserPairingHistory;
 import com.example.demo.repository.UserHashtagRepository;
 import com.example.demo.repository.UserPairingHistoryRepository;
 import com.example.demo.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
@@ -26,31 +29,16 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-//    public double calculateSimilarity(Long userId1, Long userId2) {
-//        Set<String> user1Hashtags = getUserHashtags(userId1);
-//        Set<String> user2Hashtags = getUserHashtags(userId2);
-//        return BagOfWordsAlgorithm.calculateSimilarity(user1Hashtags, user2Hashtags);
-//    }
-//
-//    private Set<String> getUserHashtags(Long userId) {
-//        Set<String> hashtags = new HashSet<>();
-//        UserHashtag userHashtag = userHashtagRepository.findByUserId(userId);
-//        if (userHashtag != null) {
-//            hashtags.add(userHashtag.getHashtag1());
-//            hashtags.add(userHashtag.getHashtag2());
-//            hashtags.add(userHashtag.getHashtag3());
-//        }
-//        return hashtags;
-//    }
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    public List<User> getAllUsersWithoutFavoriteTopicAndImage() {
-        return userRepository.getAllUsersWithoutFavoriteTopicAndImage();
-    }
+//    public List<User> getAllUsersWithoutFavoriteTopicAndImage() {
+//        return userRepository.getAllUsersWithoutFavoriteTopicAndImage();
+//    }
 
     public double calculateSimilarity(Integer userId1, Integer userId2) {
         Set<String> user1Hashtags = getUserHashtags(userId1);
@@ -83,6 +71,9 @@ public class UserService {
         userPairingHistoryRepository.save(userPairingHistory);
     }
 
-
+    @Scheduled(cron = "0 6 13 * * *" )
+    public void testScheduled(){
+        logger.info("Scheduled Function Tested");
+    }
 }
 
